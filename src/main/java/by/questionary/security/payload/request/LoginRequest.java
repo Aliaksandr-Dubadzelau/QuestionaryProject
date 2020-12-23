500d7b50bf745ef3112857ae880509eb7d241f7b
Aliaksandr Dubadzelau <aliaksandr.dubadzelau@gmail.com>
2020-12-23 23:19:52 +0300

    jwt request-response init commit

--- src/main/java/by/questionary/security/payload/request/LoginRequest.java
+++ src/main/java/by/questionary/security/payload/request/LoginRequest.java
@@ -1,4 +1,18 @@
 package by.questionary.security.payload.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
 public class LoginRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String password;
+
 }
--- src/main/java/by/questionary/security/payload/request/SignupRequest.java
+++ src/main/java/by/questionary/security/payload/request/SignupRequest.java
@@ -1,4 +1,31 @@
 package by.questionary.security.payload.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
 public class SignupRequest {
+
+    @NotBlank
+    @Length(min = 3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Length(min = 3, max = 70)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Length(min = 6, max = 50)
+    private String password;
+
+    @NotBlank
+    @Length(min = 6, max = 50)
+    private String repeatedPassword;
+
 }
